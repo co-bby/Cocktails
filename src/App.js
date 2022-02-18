16886a3f768eab175a5cb00f876844b8bdb7192c
co-bby <nkumsagoefredrick@gmail.com>
2022-02-18 09:42:38 -0500

    navbar setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 // import pages
-import Home from './pages/Home'
-import About from './pages/About'
-import SingleCocktail from './pages/SingleCocktail'
-import Error from './pages/Error'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SingleCocktail from "./pages/SingleCocktail"
+import Error from "./pages/Error"
 // import components
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar"
 function App() {
   return (
-    <div>
-      <h2>app component</h2>
-    </div>
+    <Router>
+      <Navbar />
+      <switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cocktail/:id">
+          <SingleCocktail />
+        </Route>
+        <Route exact path="*">
+          <Error />
+        </Route>
+      </switch>
+    </Router>
   )
 }
 
