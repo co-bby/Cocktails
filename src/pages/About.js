119aa024ebc2c80c517c741571925d24d23af9b9
co-bby <nkumsagoefredrick@gmail.com>
2022-02-18 11:57:21 -0500

    about page setup

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react"
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h2 className="section-title">about us</h2>
+      <p>
+        loremDolore sunt in voluptate laborum reprehenderit commodo consectetur
+        deserunt fugiat. Consequat laboris veniam laborum esse culpa anim elit
+        anim eu labore amet aute duis. Id voluptate incididunt anim dolore
+        officia reprehenderit. Aliqua aliquip consequat qui qui in quis eiusmod
+        laboris id. Quis occaecat occaecat exercitation nostrud eiusmod
+        cupidatat enim sit culpa qui labore qui.
+      </p>
+    </section>
   )
 }
 
