6e4645b1e8bc2830158ec36fecfa1aa067a260d5
co-bby <nkumsagoefredrick@gmail.com>
2022-02-23 09:22:35 -0500

    added a single coctail info feature

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -11,7 +11,7 @@ const Cocktail = ({ image, name, id, info, glass }) => {
         <h3>{name}</h3>
         <h4>{glass}</h4>
         <p>{info}</p>
-        <Link to={`/coctail/${id}`} className="btn btn-primary btn-details">
+        <Link to={`/cocktail/${id}`} className="btn btn-primary btn-details">
           details
         </Link>
       </div>
